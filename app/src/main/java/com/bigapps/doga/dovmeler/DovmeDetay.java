6f440995b2db65e809dba3ec7045c5b616c435bf
dogabaris <dogabarisozdemir@gmail.com>
2016-03-02 23:23:27 +0000

    paylaşma özelliği bitti cache kontrol edilecek reklam eklenecek

--- app/src/main/java/com/bigapps/doga/dovmeler/DovmeDetay.java
+++ app/src/main/java/com/bigapps/doga/dovmeler/DovmeDetay.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -48,7 +50,14 @@ public class DovmeDetay extends Activity{
                 .into(view);
 
         Button indir_btn = (Button) findViewById(R.id.indir_btn);
+        Button paylas_btn = (Button) findViewById(R.id.paylas_btn);
 
+        paylas_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareImage();
+            }
+        });
 
         indir_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +65,48 @@ public class DovmeDetay extends Activity{
                 imageDownload(getApplicationContext(), link);
             }
         });
+    }
+
+    private void shareImage() {
+        final ImageView imgview= (ImageView)findViewById(R.id.dovme_iv);
+
+        Uri bmpUri = getLocalBitmapUri(imgview);
+        if (bmpUri != null) {
+            // Construct a ShareIntent with link to image
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+            shareIntent.setType("image/*");
+            // Launch sharing dialog for image
+            startActivity(Intent.createChooser(shareIntent, "Dövmeni Paylaş!"));
+        } else {
+            Toast.makeText(this, "Paylaşılamadı!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 
     public static String random() {
